902b662806c17e74ebff0ffa48da3edd34b6d32b
test <fortunefi.inc@gmail.com>
2025-09-24 14:29:58 +0200

    Update script.js
    hi

--- script.js
+++ script.js
@@ -171,7 +171,7 @@
 
         ctx.rotate(Math.PI/2); // 180° draaien
         const imgSize = radius * 0.9;
-        ctx.drawImage(nftImg, -imgSize/2, 0, imgSize-2, imgSize-2);
+        ctx.drawImage(nftImg, -imgSize/2, imgSize, imgSize-2, imgSize-2);
 
         ctx.restore();
       } else {
